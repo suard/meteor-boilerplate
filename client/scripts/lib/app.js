45e49c44d572a434984dfed075ca841395682858
Suard Damme <suardd@gmail.com>
2017-03-08 19:18:54 +0100

    init commit

---
+++ client/scripts/lib/app.js
@@ -0,0 +1,29 @@
+import 'angular-animate';
+import 'angular-meteor';
+import 'angular-sanitize';
+import 'angular-ui-router';
+import 'ionic-scripts';
+import Angular from 'angular';
+import { Meteor } from 'meteor/meteor';
+
+// Modules
+
+const App = 'Mobile App';
+
+// App
+Angular.module(App, [
+    'angular-meteor',
+    'ionic'
+]);
+
+// Startup
+if (Meteor.isCordova) {
+    Angular.element(document).on('deviceready', onReady);
+}
+else {
+    Angular.element(document).ready(onReady);
+}
+
+function onReady() {
+    Angular.bootstrap(document, [App]);
+}
